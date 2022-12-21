df5b59c85cb65fe5484c0d571a6dad45bc4c0e75
Khanh Pham <116190824+khanh-bnm@users.noreply.github.com>
2022-12-21 10:08:52 +0700

    add build script

--- build_with_repo.sh
+++ build_with_repo.sh
@@ -4,9 +4,9 @@ set -eu
 
 export SERVICE=$1
 export GIT_REPO=$2
+export API_VERSION=$3
 echo "Service: $SERVICE"
 export GIT_TAG=$(git name-rev --tags --name-only $(git rev-parse HEAD))
-export API_VERSION=$(git name-rev --tags --name-only $(git rev-parse HEAD))
 export COMMIT_HASH=$(git rev-parse HEAD)
 export COMMIT_HASH_SHORT=$(git rev-parse --short HEAD)
 export BUILT_AT=$(LC_ALL=C date -u '+%d %B %Y %r (UTC)')
